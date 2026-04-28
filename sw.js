e32990a54552f2665e04a6ba3ad50092354816b8
nathanl1736 <93240660+nathanl1736@users.noreply.github.com>
2026-04-28 17:09:35 +1000

    Refine journal collage and bump app version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.8.6';
+const APP_VERSION = '1.8.7';
 const CACHE_PREFIX = 'nathans-calories-ledger';
 const CACHE_NAME = `${CACHE_PREFIX}-${APP_VERSION}`;
 const APP_SHELL = [
